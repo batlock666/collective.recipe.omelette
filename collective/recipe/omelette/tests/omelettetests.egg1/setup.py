93095f2775bb9f7d0415810674d7c5438a358534
Florian Schulze <florian.schulze@gmx.net>
2008-08-26 14:49:08 +0000

    Added history to changes.txt. Fix test for eggs which have a name different from the contents.

--- collective/recipe/omelette/tests/omelettetests.egg1/setup.py
+++ collective/recipe/omelette/tests/omelettetests.egg1/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='WeWantAnotherName',
+setup(name='egg.with.name.different.from.contents',
       packages=find_packages(),
       namespace_packages=['omelettetests'],
       install_requires=['setuptools'],
